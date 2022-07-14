0b6baf969fdb9d09ce1c5db2c5455312b5c88314
Rodrigue Coail <rodrigue.coail@tmtlsdw.com>
2022-07-14 13:11:32 -0400

    gh-pages

---
+++ src/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Card: React.FC = ( {} ) => {
+  return (<div className="card">
+    <img src="https://picsum.photos/200/300" alt=""/>
+    <div>
+      <h2>Lambda</h2>
+      <p>lambda@lambda.lambda</p>
+    </div>
+  </div>);
+};
