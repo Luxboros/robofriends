12d9113defdb192b481c4d8d39bed286464d4614
Rodrigue Coail <rodrigue.coail@tmtlsdw.com>
2022-07-15 11:50:48 -0400

    Before List

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 import { Card } from "./Card";
 
 
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+}
+
 function App() {
-  return (<Card/>);
+  const [ robots, setRobots ] = useState( [] as User[] );
+  useEffect( () => {
+    fetch( "https://jsonplaceholder.typicode.com/users" ).then( response => response.json() ).then( data => data.map( ( {
+      id,
+      name,
+      email,
+      username
+    }: User ) => ({
+      id,
+      name,
+      email,
+      username
+    }) ) ).then( ( data: User[] ) => setRobots( data ) );
+  }, [] );
+
+
+  return (robots.length ? <div> { robots.map( ( user: User ) => <Card key={ user.id } { ...user }/> ) }</div> : <div>Loading...</div>);
 }
 
 export default App;
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { User } from "./App";
 
 
-export const Card: React.FC = ( {} ) => {
-  return (<div className="card">
-    <img src="https://picsum.photos/200/300" alt=""/>
-    <div>
-      <h2>Lambda</h2>
-      <p>lambda@lambda.lambda</p>
-    </div>
-  </div>);
+export const Card: React.FC<User> = ( {
+  name,
+  id,
+  username,
+  email
+}: User ) => {
+  const set: number = Math.round( Math.random() * 6 + 1 );
+  const src: string = `https://robohash.org/${ username }?set=set${ set }`;
+  return (
+    <div className="bg-purple-400 hover:bg-purple-500 min-w-min max-w-xs min-h-min max-h-xs text-center font-mono rounded-lg divide-y-px cursor-default m-0.5">
+      <img { ...{ src } } alt="Random photo from robohash.org"/>
+      <div>
+        <h2 className="text-2xl font-bold">{ username }</h2>
+        <h4 className="text-xl">{ name }</h4>
+        <p className="text-lg font-light">{ email }</p>
+      </div>
+    </div>);
 };
